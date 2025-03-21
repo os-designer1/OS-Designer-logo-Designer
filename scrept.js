507a965f873dd354cc94c955cebc7c8e551a6a67
Your Name <Gitos5647@gmail.com>
2025-03-21 03:50:29 -0700

    الاسلايدر و تحتو

--- scrept.js
+++ scrept.js
@@ -17,3 +17,42 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+
+
+
+/* الاسلايدر */
+var swiper = new Swiper(".mySwiper", {
+    loop: false,
+   
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 20,
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+});
+/* انميشن للي تحت الاسليدر */
+document.addEventListener("DOMContentLoaded", function() {
+    const section = document.getElementById("logo-section");
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                section.classList.add("visible");
+            }
+        });
+    }, { threshold: 0.5 });
+    observer.observe(section);
+});
